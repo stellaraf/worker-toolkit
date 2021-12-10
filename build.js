cda8edb2aa9162f0f2fdfe4ec2a3ecc092ff203d
thatmattlove <mtt.lov@gmail.com>
2021-12-10 16:20:17 -0700

    exclude externals from build

--- build.js
+++ build.js
@@ -1,29 +1,47 @@
 const esbuild = require('esbuild');
 const glob = require('tiny-glob');
+const fs = require('fs');
+
+function getExternals() {
+  const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+  return Array.from(
+    new Set(
+      Object.keys(
+        Object.assign(
+          {},
+          pkg.dependencies || {},
+          pkg.devDependencies || {},
+          pkg.peerDependencies || {},
+        ),
+      ),
+    ),
+  );
+}
 
 /**
  * Transpile and build from source.
  */
 async function build() {
   const entryPoints = await glob('./src/**/!(*.test|*types).ts');
+  const external = getExternals();
 
   /** @type import("esbuild").BuildOptions */
   const options = {
     outdir: './dist',
-    bundle: true,
     format: 'esm',
+    bundle: true,
     minify: false,
     sourcemap: false,
     logLevel: 'error',
-    target: ['esnext', 'node12'],
+    target: 'node12',
     treeShaking: true,
     platform: 'node',
     entryPoints,
-    external: ['worktop'],
+    external,
   };
   try {
     await esbuild.build(options);
-    console.log(`✅  Build complete => 'dist/index.js'`);
+    console.log(`✅  Build complete`);
     process.exit(0);
   } catch (err) {
     console.error(err);
